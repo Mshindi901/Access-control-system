76e1f2ffe8f479137ff962c9f785960838bf3bbe
emmanuel mshindi <mshindi@kali.mshindi>
2026-09-11 14:53:43 +0300

    Added the Internal database url for render

--- src/Database/config.js
+++ src/Database/config.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 
-const sequelize = new Sequelize('access', 'postgres', 'yugah2005@', {
+const sequelize = new Sequelize('postgresql://access_db_5p78_user:7l8zTZZBkF4YZ6xNAkH972Va63nuGIg1@dpg-dahun23m8hqs73df79sg-a/access_db_5p78', {
     host: 'localhost',
     dialect: 'postgres'
 });
